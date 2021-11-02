3a90df778b165c0b7016d8b6e081d1390b15d83e
久保田千尋 <kubotachihiro@kubotachihironoMacBook-Pro.local>
2021-11-02 18:00:29 +0900

    修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,6 +89,26 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
     end
+    it '姓（全角）に半角文字が含まれていると登録できない' do
+      @user.family_name_kana ='12345'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Family name kana is invalid')
+    end
+    it '名（全角）に半角文字が含まれていると登録できない' do
+      @user.first_name_kana ='12345'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('First name kana is invalid')
+    end
+    it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+      @user.password ='ひらがな漢字abc123@'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+    end
+    it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+      @user.password ='ひらがな漢字abc123@'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+  end
   end
   end
 end
