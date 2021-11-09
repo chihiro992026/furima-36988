5a258f694c2c5a7da844d0ad165391b118ee5dfb
久保田千尋 <kubotachihiro@kubotachihironoMacBook-Pro.local>
2021-11-09 18:41:27 +0900

    コミット１

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,10 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
   def products_params
     params.require(:product).permit(:category_id, :product_name, :description, :prefecture_id, :shipping_cost_id, :shipping_days_id, :status_id, :price, :image).merge(user_id: current_user.id)
